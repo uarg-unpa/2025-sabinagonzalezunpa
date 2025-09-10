3acb5b83892afaa79cc498d050e2cc936c4378f0
sabinagonzalezunpa <gonzalezsabina532@gmail.com>
2025-09-10 19:57:40 +0000

    grupo

--- tp1/clases.java/testPelicula.java
+++ tp1/clases.java/testPelicula.java
@@ -1,6 +1,6 @@
 public class testPelicula{
-    public static void main(String [] args)
-    {
+
+    public static void main(String [] args){
     Pelicula p1 = new Pelicula( " Mi pobre angelito", " Chris Columbus" , "Comedia", 1991 , 20 , 2 );
     Pelicula p2 = new Pelicula ( "Top Gun: Maverick" , " Joseph Kosinski" , "Accion", 2022 , 13 , 2 );
  
