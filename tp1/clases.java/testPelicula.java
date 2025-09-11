d6ef4c088bf9a7589793f9270d9f02fd6dd477bd
sabinagonzalezunpa <gonzalezsabina532@gmail.com>
2025-09-11 15:25:01 +0000

    grupo4

--- tp1/clases.java/testPelicula.java
+++ tp1/clases.java/testPelicula.java
@@ -1,12 +1,52 @@
 import java.util.Scanner;
 public class testPelicula{
 
+    public static void main(String [] args){
     Scanner scan= new Scanner(System.in);
     
+        System.out.println("Ingrese título de la película:");
+        String titulo1 = scanner.nextLine();
 
-    public static void main(String [] args){
+        System.out.println("Ingrese director de la película:");
+        String director1 = scanner.nextLine();
+
+        System.out.println("Ingrese género de la película:");
+        String nombreGenero1 = scanner.nextLine();
+
+        System.out.println("Ingrese el añoDePublicacion de la pelicula:");
+        int añoDePublicacion1 = scanner.nextLine();
+
+        System.out.println("Ingrese el contenidopelicula de la pelicula:");
+        int contenidopelicula1 = scanner.nextLine();
+
+        System.out.println("Ingrese la duracion de la pelicula:");
+        int duracion1 = scanner.nextLine();
+        scanner.nextLine();
+
     Pelicula p1 = new Pelicula( " Mi pobre angelito", " Chris Columbus" , "Comedia", 1991 , 20 , 2 );
+    Genero g1 = new Genero("");
+    
+        System.out.println("Ingrese título de la película:");
+        String titulo2 = scanner.nextLine();
+
+        System.out.println("Ingrese director de la película:");
+        String director2 = scanner.nextLine();
+
+        System.out.println("Ingrese género de la película:");
+        String nombreGenero2 = scanner.nextLine();
+
+        System.out.println("Ingrese el añoDePublicacion de la pelicula:");
+        int añoDePublicacion2 = scanner.nextLine();
+
+        System.out.println("Ingrese el contenidopelicula de la pelicula:");
+        int contenidopelicula2 = scanner.nextLine();
+
+        System.out.println("Ingrese la duracion de la pelicula:");
+        int duracion2 = scanner.nextLine();
+        scanner.nextLine();
+
     Pelicula p2 = new Pelicula ( "Top Gun: Maverick" , " Joseph Kosinski" , "Accion", 2022 , 13 , 2 );
+    Genero g2 = new Genero("");
  
     System.out.println("Pelicula 1: " + p1.mostrarPelicula());
     System.out.println("Pelicula 2:" + p2.mostrarPelicula());
