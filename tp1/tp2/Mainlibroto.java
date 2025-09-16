58e7823e825b62cb55fd6ac990b4824487c10cc5
sabinagonzalezunpa <gonzalezsabina532@gmail.com>
2025-09-16 00:49:10 +0000

    tp2

---
+++ tp1/tp2/Mainlibroto.java
@@ -0,0 +1,8 @@
+package tp1.tp2;
+
+public class Mainlibroto {
+    public static void main(String []args){
+        Libro lib1=new Libro("2015", "El señor de los anillos", "J. R. R. Tolkien", "Minotauro", "25 de mayo de 2022" );
+    }
+    
+}
