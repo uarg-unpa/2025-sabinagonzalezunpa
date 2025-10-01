544caef12f040fb8826151a6f39767417b6f047b
sabinagonzalezunpa <gonzalezsabina532@gmail.com>
2025-10-01 19:06:15 +0000

    colas

--- Colas.java/ColaInt.java
+++ Colas.java/ColaInt.java
@@ -1,6 +1,7 @@
 public class ColaInt {
     private int [] elementos;
     private int frente;
+    private int fin;
     private final int MAX = 10;
 
     public ColaInt (){
@@ -9,9 +10,22 @@ public class ColaInt {
         fin = 0;
     }
     public boolean estaVacia(){
-        return fin == 0;
+        return fin == 0; // Vacia si no se ha encolado nada 
     }
     public boolean estaLlena(){
-        return fin == MAX; 
+        return fin == MAX; // Llena si fin llega al final
+    }
+    public void encolar(int elem){
+        elementos[fin] = elem;
+        fin++;
+    }
+    public int desencolar(){
+        int aux = elementos[frente]; // toma el primer elemento 
+        // Desplaza los elementos restantes hacia la izquierda
+        for (int i = 0 ; i < fin -1 ; i++){
+            elementos[i] = elementos [i + 1];
+        }
+        fin--; // Reduce fin tras el desplazamiento
+        return aux;
     }
 }
