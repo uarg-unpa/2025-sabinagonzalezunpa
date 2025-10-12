fe05d53ce74471f06463553c60b26b1c4650ad08
sabinagonzalezunpa <gonzalezsabina532@gmail.com>
2025-10-12 16:19:51 +0000

    main elector

--- ejercicio elector/MainElector.java
+++ ejercicio elector/MainElector.java
@@ -14,8 +14,15 @@ public class MainElector {
         System.out.println("Ingrese la clase del elector 1:");
         int clase1 = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Ingrese el domicilio del elector 1:");
-        Domicilio domicilio1 = scanner.nextLine();
+        System.out.println("Ingrese la calle del domicilio del elector 1:");
+        String calle1 = scanner.nextLine();
+        System.out.println("Ingrese el número del domicilio:");
+        int numero1 = scanner.nextInt();
+        scanner.nextLine();
+
+
+        Domicilio domicilio1 = new Domicilio(calle1, numero1);
+
 
         Elector e1 = new Elector (nombre1 , apellido1 , matricula1 , clase1 , domicilio1);
 
@@ -29,8 +36,14 @@ public class MainElector {
         System.out.println("Ingrese la clase del elector 2:");
         int clase2 = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Ingrese el domicilio del elector 2:");
-        Domicilio domicilio2 = scanner.nextLine();
+        System.out.println("Ingrese la calle del domicilio del elector 2:");
+        String calle2 = scanner.nextLine();
+        System.out.println("Ingrese el número del domicilio:");
+        int numero2 = scanner.nextInt();
+        scanner.nextLine();
+
+
+        Domicilio domicilio2 = new Domicilio(calle2, numero2);
 
         Elector e2 = new Elector (nombre2 , apellido2 , matricula2 , clase2 , domicilio2);
 
@@ -44,8 +57,14 @@ public class MainElector {
         System.out.println("Ingrese la clase del elector 3:");
         int clase3 = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Ingrese el domicilio del elector 3:");
-        Domicilio domicilio3 = scanner.nextLine();
+        System.out.println("Ingrese la calle del domicilio del elector 3:");
+        String calle3 = scanner.nextLine();
+        System.out.println("Ingrese el número del domicilio:");
+        int numero3 = scanner.nextInt();
+        scanner.nextLine();
+
+
+        Domicilio domicilio3 = new Domicilio(calle3, numero3);
 
         Elector e3 = new Elector (nombre3 , apellido3 , matricula3 , clase3 , domicilio3);
 
@@ -59,8 +78,14 @@ public class MainElector {
         System.out.println("Ingrese la clase del elector 4:");
         int clase4 = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Ingrese el domicilio del elector 4:");
-        Domicilio domicilio4 = scanner.nextLine();
+        System.out.println("Ingrese la calle del domicilio del elector 4:");
+        String calle4 = scanner.nextLine();
+        System.out.println("Ingrese el número del domicilio:");
+        int numero4 = scanner.nextInt();
+        scanner.nextLine();
+
+
+        Domicilio domicilio4 = new Domicilio(calle4, numero4);
 
         Elector e4 = new Elector (nombre4 , apellido4 , matricula4 , clase4 , domicilio4);
 
@@ -74,8 +99,14 @@ public class MainElector {
         System.out.println("Ingrese la clase del elector 5:");
         int clase5 = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Ingrese el domicilio del elector 5:");
-        Domicilio domicilio5 = scanner.nextLine();
+        System.out.println("Ingrese la calle del domicilio del elector 5:");
+        String calle5 = scanner.nextLine();
+        System.out.println("Ingrese el número del domicilio:");
+        int numero5 = scanner.nextInt();
+        scanner.nextLine();
+
+
+        Domicilio domicilio5 = new Domicilio(calle5, numero5);
 
         Elector e5 = new Elector (nombre5 , apellido5 , matricula5 , clase5 , domicilio5);
 
@@ -97,5 +128,4 @@ public class MainElector {
 
         scanner.close();
     }
-    
 }
